7868ab8f6236672f2883e56e0d472614b12fc6b1
Gabriel Rodrigues <gabrielrodrigues1927@gmail.com>
2024-03-12 10:48:12 -0300

    Implements mapStruct

---
+++ src/main/java/com/rodrigues/propappstudy/mapper/ProposalMapper.java
@@ -0,0 +1,37 @@
+package com.rodrigues.propappstudy.mapper;
+
+import com.rodrigues.propappstudy.dtos.proposal.ProposalRequestDto;
+import com.rodrigues.propappstudy.dtos.proposal.ProposalResponseDto;
+import com.rodrigues.propappstudy.entity.Proposal;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.util.Iterator;
+import java.util.List;
+
+@Mapper
+public interface ProposalMapper {
+
+    ProposalMapper INSTANCE = Mappers.getMapper(ProposalMapper.class);
+
+    @Mapping(target = "user.name", source = "name")
+    @Mapping(target = "user.lastName", source = "lastName")
+    @Mapping(target = "user.cpf", source = "cpf")
+    @Mapping(target = "user.phone", source = "phone")
+    @Mapping(target = "user.income", source = "income")
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "isApproved", ignore = true)
+    @Mapping(target = "integrated", ignore = true)
+    @Mapping(target = "observation", ignore = true)
+    Proposal convertDtoToProposal(ProposalRequestDto requestDto);
+
+    @Mapping(target = "name", source = "user.name")
+    @Mapping(target = "lastName", source = "user.lastName")
+    @Mapping(target = "cpf", source = "user.cpf")
+    @Mapping(target = "phone", source = "user.phone")
+    @Mapping(target = "income", source = "user.income")
+    ProposalResponseDto convertEntityToDto(Proposal proposal);
+
+    List<ProposalResponseDto> convertListEntityToListDto(List<Proposal> proposalIterator);
+}
